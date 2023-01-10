bb67fb8b59a9ee164f25a9fe2ee52d1449bf392f
Milton Ly <ly.milton@gmail.com>
2023-01-09 17:57:19 -0800

    time added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDayEl = document.getElementById('currentDay');
+
+const date = new Date();
+let day = date.getDate();
+let month = date.getMonth()+1;
+let year = date.getFullYear;
+console.log(date);
+currentDayEl.innerText = date;
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
